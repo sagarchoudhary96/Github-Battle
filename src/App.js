cd8be82337a9778cf17ab7e4a984370e24c55fca
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-07-15 23:27:53 +0530

    Add: Home and Nav Components and routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Popular from './Popular.js'
 import { Route } from 'react-router-dom'
+import Nav from './nav.js'
+import Home from './Home'
+
 class App extends Component {
   render() {
     return (
       <div className="container">
-      <Route path='/popular' render = {()=> (
-          <Popular/>
-        )}/>
-    </div>  
+        <Nav/>
+        <Route exact path='/' render = {()=>(
+            <Home />
+          )}/>
+
+        <Route path='/popular' render = {()=> (
+            <Popular/>
+          )}/>
+      </div>
     )
   }
 }
--- src/nav.js
+++ src/nav.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
 import { PropTypes } from 'prop-types'
+import { NavLink }from 'react-router-dom'
 
-function nav() {
+function Nav() {
   return (
     <ul className="nav">
-      
+      <li>
+        <NavLink
+          exact activeClassName='active'
+          to = '/'>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          activeClassName='active'
+          to = '/battle'>
+          Battle
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          activeClassName='active'
+          to = '/popular'>
+          Popular
+        </NavLink>
+      </li>
     </ul>
   )
 }
 
-export default nav
+export default Nav
